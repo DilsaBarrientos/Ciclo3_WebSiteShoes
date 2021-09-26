6bfacbe28750a9b236b31c82f8a0716d61d69625
RENATA VELEZ <renavesa26@hotmail.com>
2021-09-26 18:39:00 -0500

    validación telefono renata ok

--- js/form.js
+++ js/form.js
@@ -18,6 +18,27 @@ function checkDir(event) {
 }
 module.exports = checkDir;
 
+//validación telefono por Renata
+function checkTelefono(event) {
+  let key = event.keycode || event.which;
+  let tecla = String.fromCharCode(key).toString();
+  let telefono = document.getElementById("telefono").value;
+  let numeros = "0123456789";
+  
+  if (numeros.indexOf(tecla) == -1 ) {
+    alert("Solo se admiten números: ");
+    return false;
+  }
+  else if( telefono.length < 7) {
+    alert("Los telefonos fijos deben tener exactamente 7 dígitos");
+    return false;
+  }
+  else{
+    return true
+  }
+}
+module.exports = checkTelefono;
+
 
 
 
