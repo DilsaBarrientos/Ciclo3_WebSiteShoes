b315052a9a2a915feb92d8b3ea65d22d09079fd9
LuisMiguelMendozaGuevara <lmmg.1201@gmail.com>
2021-10-17 19:45:05 -0400

    version con css de login f y con el login ilegal para el bot

--- js/registro.js
+++ js/registro.js
@@ -1,13 +1,13 @@
 let registros = [];
 
-function agregarRegistro() {
+function agregarRegistro(){
     var nombre = document.getElementById('nombre').value;
     var genero = document.getElementById('genero').value;
     var telefono = document.getElementById('telefono').value;
     var direccion = document.getElementById('direccion').value;
     var correo = document.getElementById('correo').value;
     var contrasena = document.getElementById('contrasena').value;
-    function Registro(nombre, genero, telefono, direccion, correo, contrasena) {
+    function Registro(nombre,genero,telefono,direccion,correo,contrasena){
         this.nombre = nombre;
         this.genero = genero;
         this.telefono = telefono;
@@ -15,36 +15,26 @@ function agregarRegistro() {
         this.correo = correo;
         this.contrasena = contrasena;
     }
-    var registro_usuario = new Registro(nombre, genero, telefono, direccion, correo, contrasena);
+    var registro_usuario = new Registro(nombre,genero,telefono,direccion,correo,contrasena);
     registros.push(registro_usuario);
     console.log(registros);
 }
-function ordenarArreglo(arreglo) {
-    let  ordenar = arreglo.sort((a, b) => {
-        if (a.nombre < b.nombre) return -1;
-        if (a.nombre > b.nombre) return 1;
-        return 0;
-    });
-    console.log(ordenar);
-    return ordenar;
-}
 
-function filtrarCorreo(arreglo) {
-    let arregloFiltrado = [];
-    let filtrar = /@gmail.com/
-    for (x = 0; x < arreglo.length; x++) {
-        if (filtrar.test(arreglo[x].correo)) {
+function filtrarCorreo(arreglo){
+    let arregloFiltrado=[];
+    let filtrar=/@gmail.com/
+    for (x=0;x<arreglo.length;x++) {
+        if(filtrar.test(arreglo[x].correo)){
             arregloFiltrado.push(arreglo[x].correo);
         }
-
-    }
+            
+            
+        }
     console.log(arregloFiltrado);
-    return arregloFiltrado;
-}
-module.exports.registros = registros
-module.exports.ordenarArreglo= ordenarArreglo;
-module.exports.agregarRegistro = agregarRegistro;
-module.exports.filtrarCorreo = filtrarCorreo;
+    return arregloFiltrado;}
+    module.exports.registros = registros;
+    module.exports.agregarRegistro = agregarRegistro;
+    module.exports.filtrarCorreo=filtrarCorreo;
 
 
 
